073a0bb25c5487a92c806db91c368e7bbb3f6168
max <meison98@bk.ru>
2019-06-13 16:27:48 +0300

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+
+    render() {
+
+        return(
+           <div>
+               Hello, World !!!
+           </div>
+        )
+    }
+
+}
+
+export default App;
